03455534878c42281db934f91f8d4d2b9212a2bf
Avijit Biswas <avijitb440@gmail.com>
2021-01-18 00:53:02 +0530

    html,scss,javascript complete project

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  if (header.classList.contains("open")) {
+    // Close Hamburger menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-in");
+      elem.classList.add("fade-out");
+    });
+  } else {
+    // Open Hamburger menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-out");
+      elem.classList.add("fade-in");
+    });
+  }
+});
